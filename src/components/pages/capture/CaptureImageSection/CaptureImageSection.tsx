9e91c795d35cca319aeb15c4fa6643ac711c54d3
alex.matos <alex.matos@ceuma.br>
2022-04-10 22:52:27 -0300

    feat: fazendo upload de imagem

--- src/components/pages/capture/CaptureImageSection/CaptureImageSection.tsx
+++ src/components/pages/capture/CaptureImageSection/CaptureImageSection.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import Webcam from "react-webcam";
+import { useRouter } from "next/router";
 import { FiArrowRight } from "react-icons/fi";
 import { useCallback, useRef, useState } from "react";
-import Webcam from "react-webcam";
-import * as S from "./CaptureImageSection.styles";
-import axios from "axios";
+import { notify } from "@/utils/helpers/notify.helper";
+import { api } from "@/utils/services/axios.service";
+
 import { Loading } from "@/components/shared/form";
-import { notify } from "@/utils/helpers/notify";
-import { useRouter } from "next/router";
+
+import * as S from "./CaptureImageSection.styles";
 
 const videoConstraints = {
   width: 500,
@@ -19,38 +21,34 @@ export function CaptureImageSection() {
   const { push } = useRouter()
 
   const [isLoading, setIsLoading] = useState(false);
-  // const [image, setImage] = useState('');
 
   const capture = useCallback(async () => {
-    // const imageSrc = webcamRef.current.getScreenshot();
-
-    // setImage(imageSrc)
-
     setIsLoading(true)
 
-    setTimeout(() => {
-      push('/')
-    }, 2000)
-  }, [push]);
-
-  // async function handleSubmitImage() {
-    // try {
-    //   const { data } = await axios.post("/api/image", formData);
-
-    //   console.log(data);
-    // } catch (err) {
-    //   console.log(err);
-    //   notify("error", "Ocorreu um erro ao buscar este usuário");
-    // } finally {
-    //   setIsLoading(false);
-    // }
-
-    // const formData = new FormData();
-
-    // formData.append("file", image)
-
-    // setIsLoading(true)
-  // }
+    webcamRef.current.canvas.toBlob(async (blob: Blob) => {
+      try {
+        const formData = new FormData()
+  
+        formData.append('files', blob)
+  
+        const { data } = await api.post("/person/identify", formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          },
+          params: {
+            groupId: 'prisioners_02'
+          }
+        });
+  
+        console.log(data);
+      } catch (err) {
+        console.log(err);
+        notify("error", "Ocorreu um erro ao buscar este usuário");
+      } finally {
+        setIsLoading(false);
+      }
+    })
+  }, []);
 
   return (
     <S.Container>
@@ -63,8 +61,8 @@ export function CaptureImageSection() {
       <S.WebcamContainer>
         <Webcam
           ref={webcamRef}
-          width={300}
-          height={200}
+          width={400}
+          height={250}
           audio={false}
           screenshotQuality={1}
           screenshotFormat="image/jpeg"
@@ -76,7 +74,7 @@ export function CaptureImageSection() {
         </S.Description>
 
         <S.Button onClick={capture}>
-          <p>Tirar foto</p>
+          <p>Buscar informações</p>
 
           <FiArrowRight size={20} />
         </S.Button>
